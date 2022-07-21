4ad785b5d3034c2f638994e8c3f80a28b0cf1545
MICHEL NEGRAO <michelnegrao@gmail.com>
2022-07-20 23:09:33 -0300

    Nao consegui acertar o getSessionData em react

--- react/GetSessionData.tsx
+++ react/GetSessionData.tsx
@@ -1,45 +1,103 @@
-import React, { FC } from 'react'
+import React,{ useEffect, useState} from 'react'
 import { useFullSession } from 'vtex.session-client'
 import { useQuery } from 'react-apollo'
-import queryClientWallet from './graphql/getWallet.gql'
+import QUERY_CLIENT_WALLET from './graphql/getWallet.gql'
 
 
-const GetSessionData: FC = () => {
-  const { loading, data: sessionData, error } = useFullSession()
+const showBalance = () =>  {
+  
+  
+  const [sessionUserId, setSessionUserId] = useState();
+  
+  const {  loading ,data  : dataSession, error} = useFullSession({
+    variables: {
+      items: ['profile.id.value']
+    },
+    onCompleted: () => {
+      setSessionUserId(dataSession.session.id);      
+      
+      
+    },
+  })
+  
+  if (loading) {
+    return 'Loading…'
+  }
+  if (error) {
+    return `Error ${error}`
+  }
 
+    useEffect(() => {
+      
+      const { data : wallet} = useQuery(QUERY_CLIENT_WALLET,{ variables: { iduser:  sessionUserId }})
+      const [walletBalance,setWalletBalance] = useState()
+      setWalletBalance(wallet)      
+      console.log('walletBalance',walletBalance)
+      
+    })
+  
+  
+  //useEffect(function getWallet() {
+  
+  //const { data }  =
+
+
+ //});
+
+
+
+
+  
+    //const [sessionData, setsessionData] = useState(data);
+    //console.log('sessionData',sessionData)
+    //setsessionData(sessionData);
+
+
+  return <>0</>
+
+}
+
+export default showBalance
+
+/*
   if (loading) {
     return <>Session is loading</>
   }
 
   if (error) {
     return <>Session has errors</>
-  }
-
-  const handleToggle = () => {
-   return  useQuery(queryClientWallet);
-  }
-
-  try { console.log(handleToggle()) } catch (e) {
-    console.log(e)
-  }
-  let userId = sessionData?.session.namespaces.profile.id.value
+  } */
+  //console.log(sessionData)
 
 
-
-  //const { data: walletdata } =  useQuery(queryClientWallet)
-  //let balance = walletdata?.queryClientWallet[0].balance
-  //let balance = 25
-
-  console.log(userId)
-  //console.log(walletdata)
-  //console.log(balance)
-
-  //console.log(useQuery(hardcodedwallet))
+/*   return sessionData
 
 
-  //return <>${balance}</>
-  return <>Ready</>
-}
+ try {
+
+  console.log('FC',showBalance().session)
+  const {data : qryresult} = useQuery(queryClientWallet,{ variables: { iduser: showBalance().session.id }})
+  //console.log('showbalance',qryresult)
+} catch (e){console.log(e)}
 
 
-export default GetSessionData
+
+return <>0</> */
+
+
+/* const GetSessionData: FC = () => {
+} */
+
+
+
+/*   const testWallet = {
+    "data": {
+      "queryClientWallet": [
+        {
+          "id": "bedf7a89-0716-11ed-835d-0e41ba39b31f",
+          "userId": "01864ade-2de1-4916-adf2-32ada24e9fc4",
+          "balance": "39"
+        }
+      ]
+    }
+  } */
